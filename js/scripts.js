4aca5f253098674a9818492834be791fd4e95ca2
j300-174 <jeddmarpics@icloud.com>
2021-02-25 18:36:03 +0000

    - Added form and submit details feature using js - aria-label for screen readers for buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log("hello");
+// console.log("hello");
+// event.preventDeafault prevents submit btn from submitting a form default action...
+// as we're not sending to a database...
+
+const msgContent = "";
+
+function msgSent(event) {
+  sent.textContent = 'Thank you for messaging!';
+  event.preventDefault();
+};
+
+const form = document.getElementById('form');
+const sent = document.getElementById('sent');
+
+form.addEventListener('submit', msgSent);
