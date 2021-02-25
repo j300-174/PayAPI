ca0e962fffa594698a062e75d33d57703990596f
j300-174 <jeddmarpics@icloud.com>
2021-02-25 18:40:42 +0000

    refactored msgSent code

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,8 @@
-// console.log("hello");
 // event.preventDeafault prevents submit btn from submitting a form default action...
 // as we're not sending to a database...
-
 const msgContent = "";
 
-function msgSent(event) {
-  sent.textContent = 'Thank you for messaging!';
-  event.preventDefault();
-};
-
-const form = document.getElementById('form');
-const sent = document.getElementById('sent');
-
-form.addEventListener('submit', msgSent);
+form.addEventListener('submit', (event) => {
+    sent.textContent = "thank you for messaging";
+    event.preventDefault();
+});
