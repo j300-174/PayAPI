36fbe9532a51828d37cfe4039843ddef698dfede
j300-174 <jeddpicones@gmail.com>
2021-10-20 12:46:30 +0000

    removed extra files unused

--- js/FAQ.js
+++
@@ -1,5 +0,0 @@
-console.log('faq');
-
-FAQ1.textContent = "hello";
-
-FAQ1.appendChild(FAQ1);
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
-// event.preventDeafault prevents submit btn from submitting a form default action...
-// as we're not sending to a database...
-// console.log('testing');
-// const msgContent = "";
-
-// form.addEventListener('submit', (event) => {
-//     sent.textContent = "thank you for messaging";
-//     event.preventDefault();
-// });
-
 menuToggler.addEventListener('click', ev => {
   menu.classList.toggle('open');
 });
